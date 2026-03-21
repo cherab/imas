df861b884098c82ca3d705f29504b517ef12c1a5
munechika-koyo <munechika.koyo@gmail.com>
2026-03-21 01:37:07 +0100

    ✨ Add fixtures for SOLPS and JOREK IMAS datasets; refactor import for Vector3DFunction2D

--- tests/plasma/conftest.py
+++ tests/plasma/conftest.py
@@ -3,13 +3,31 @@ from pathlib import Path
 
 import pytest
 
-from cherab.imas.datasets import iter_jintrac
+from cherab.imas.datasets import iter_jintrac, iter_jorek, iter_solps
 
 
 @pytest.fixture(scope="session")
 def path_iter_jintrac(tmp_path_factory) -> str:
     """Fixture to provide the path to a sample JINTRAC IMAS dataset."""
     path = Path(iter_jintrac())
+    tmp_path = tmp_path_factory.mktemp("cherab-imas-data")
+    shutil.move(path, tmp_path)
+    return str(tmp_path / path.name)
+
+
+@pytest.fixture(scope="session")
+def path_iter_solps(tmp_path_factory) -> str:
+    """Fixture to provide the path to a sample SOLPS IMAS dataset."""
+    path = Path(iter_solps())
+    tmp_path = tmp_path_factory.mktemp("cherab-imas-data")
+    shutil.move(path, tmp_path)
+    return str(tmp_path / path.name)
+
+
+@pytest.fixture(scope="session")
+def path_iter_jorek(tmp_path_factory) -> str:
+    """Fixture to provide the path to a sample JOREK IMAS dataset."""
+    path = Path(iter_jorek())
     tmp_path = tmp_path_factory.mktemp("cherab-imas-data")
     shutil.move(path, tmp_path)
     return str(tmp_path / path.name)
--- tests/plasma/test_equilibrium.py
+++ tests/plasma/test_equilibrium.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from raysect.core.math.function.vector3d import FloatToVector3DFunction2D
+from raysect.core.math.function.vector3d import Function2D as Vector3DFunction2D
 
 from cherab.imas.plasma.equilibrium import load_equilibrium, load_magnetic_field
 from cherab.tools.equilibrium import EFITEquilibrium, plot_equilibrium
@@ -23,4 +23,4 @@ def test_load_magnetic_field(path_iter_jintrac: str):
     magnetic_field = load_magnetic_field(path_iter_jintrac, "r")
 
     # Test that Vector3DFunction2D object is returned
-    assert isinstance(magnetic_field, FloatToVector3DFunction2D)
+    assert isinstance(magnetic_field, Vector3DFunction2D)
